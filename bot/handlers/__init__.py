fe6fe2fea304b37c35f2ec762b946c041935d12e
maze <invoker322sf@gmail.com>
2023-01-02 22:17:44 +1000

    add link data

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -7,6 +7,7 @@ from bot.loader import dp
 from bot.states import CreateLink
 from bot.utils.misc import is_correct_link, is_correct_source
 from logger import logger
+from settings import BASE_URL
 
 
 @dp.message_handler(commands=['start'], state="*")
@@ -30,7 +31,7 @@ async def show(message: types.Message):
 async def get_link_info(callback_query: types.CallbackQuery, state: FSMContext):
     print(callback_query.data)
     link_id = int(callback_query.data.split("LINK_")[1])
-    return await callback_query.message.answer(f"https://localhost:6575/api/link_info/{link_id}")
+    return await callback_query.message.answer(f"https://{BASE_URL}/api/link_info/{link_id}")
 
 
 @dp.callback_query_handler(lambda cb: cb.data == 'cancel', state="*")
@@ -45,7 +46,7 @@ async def cancel(callback_query: types.CallbackQuery, state: FSMContext):
 @dp.callback_query_handler(lambda cb: cb.data == 'create_link')
 async def create_link_callback(callback_query: types.CallbackQuery, state: FSMContext):
     await CreateLink.waiting_redirect_url.set()
-    await callback_query.message.edit_text("Введите ссылку, которая будет после https://site.ru/")
+    await callback_query.message.edit_text(f"Введите ссылку, которая будет после https://{BASE_URL}/")
     return await callback_query.message.edit_reply_markup(reply_markup=CANCEL_KEYBOARD)
 
 
@@ -77,7 +78,7 @@ async def waiting_source(message: types.Message, state: FSMContext):
 
     request_data = await create_link(link, redirect)
 
-    await message.answer(f"Ссылка создана. http://{request_data[0]['link']}")
+    await message.answer(f"Ссылка создана. https://{request_data[0]['link']}")
     return await message.answer("Hello", reply_markup=START_KEYBOARD)
 
 
