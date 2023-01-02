46688563db5fa3e101e323261d1c8319e399e144
maze <invoker322sf@gmail.com>
2023-01-02 22:27:45 +1000

    remove debug

--- run.py
+++ run.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     logger.info("Starting app")
-    app.run(port=6575, debug=True)
-    #serve(app, port=6575)
+    #app.run(port=6575, debug=True)
+    serve(app, port=6575)
 
 
